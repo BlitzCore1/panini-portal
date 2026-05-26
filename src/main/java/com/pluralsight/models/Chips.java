d2c076d7ba67939ecd38844d52fb00b7c7847c7c
damarriusthompson <dthompson1@my.yearupunited.org>
2026-05-25 19:06:13 -0700

    updated Chips.java and Drink.java

--- src/main/java/com/pluralsight/models/Chips.java
+++ src/main/java/com/pluralsight/models/Chips.java
@@ -4,15 +4,25 @@ public class Chips implements OrderItem
 {
     private String type;
 
+    public Chips(String type)
+    {
+        this.type = type;
+    }
+
     @Override
     public double getPrice()
     {
-        return 0;
+        return 1.50;
     }
 
     @Override
     public String getReceiptLine()
     {
-        return "";
+        return type + " chips - $" + getPrice();
+    }
+
+    public String getType()
+    {
+        return type;
     }
 }
--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -3,17 +3,31 @@ package com.pluralsight.models;
 public class Drink implements OrderItem
 {
     private String size;
-    private String flavor;
+
+    public Drink(String size)
+    {
+        this.size = size;
+    }
 
     @Override
     public double getPrice()
     {
-        return 0;
+        return switch (this.size) {
+            case "Small" -> 2.00;
+            case "Medium" -> 2.50;
+            case "Large" -> 3.00;
+            default -> 0;
+        };
     }
 
     @Override
     public String getReceiptLine()
     {
-        return "";
+        return size + " drink - $" + getPrice();
+    }
+
+    public String getSize()
+    {
+        return size;
     }
 }
